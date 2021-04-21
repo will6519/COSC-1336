760ca10c3a500a1c7a66b0fa7a82dd3040eedf84
will6519 <81393194+will6519@users.noreply.github.com>
2021-04-20 22:30:25 -0500

    Add files via upload

---
+++ homeworkpt3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int min= 1;
+    int max= 100;
+    //Questions to Ask
+    std::cout << "Lets play a game...\n" << endl;
+    std::cout << "Think of a number between 1 and 100. \n";
+    // Meanings
+    std::cout << "c = correct  l = lower   h = higher\n";
+    std::cout << "Is it 50?\n";
+
+    int(0)
+
+}
+
