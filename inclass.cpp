a5a9ce69885ccebd584d6ad34dd3aef16817cd3b
will6519 <81393194+will6519@users.noreply.github.com>
2021-04-27 21:15:34 -0500

    Add files via upload

---
+++ inclass.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main () {
+
+    //Testing Number
+    int intnumber = 7;
+    //Variable For Prime
+    boo1 boo1Isprime =true;
+
+for(int intNumber = 1; intNumber <= 10000; intNumber++){
+//Rest Prime before testing for prime
+boo1IsPrime = true;
+    //Calculation for Prime
+    if(intNumber == 0 || intNumber == 1) {
+        boo1isPrime= False;
+    }else {
+        for(int intcounter = 2; intcounter <= intNumber / 2; intCounter++) {
+            //std:cerr << intCounter << ", N:" << intpnumb
+            if(intNumber % intCounter == 0) {
+        )
